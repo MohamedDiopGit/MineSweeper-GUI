e013e49230222020e32de6b06a04659b77431131
groot <diopm.contact@gmail.com>
2022-09-19 23:21:17 +0200

    project:quick fix

--- JavaDoc-complient/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Field"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"Levels"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
